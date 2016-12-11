791fe6baa04f5c4c392acbe83d04407132c01719
David Krénar <dawe484@gmail.com>
2016-12-11 22:46:59 +0100

    Login System with Passport v1
    Change login system to login system with passport - not yet. Now create
    express handlebars for html pages (index).

--- app.js
+++ app.js
@@ -1,151 +1,227 @@
 'use strict'
 
+let colors = require('colors/safe');
+
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let expressHandlebars = require('express-handlebars');
+let expressValidator = require('express-validator');
+let flash = require('connect-flash');
+let expressSession = require('express-session');
+let passport = require('passport');
+let localStrategy = require('passport-local').Strategy;
+
 let mongojs = require("mongojs");
 let db = mongojs('localhost:27017/2DHTML5Game', ['account']); // specified all collections in db !!
 
-let express = require('express');
+let routes = require('./routes/index');
+let accounts = require('./routes/accounts');
+
+// Init App
 let app = express();
-let serv = require('http').Server(app);
+//let serv = require('http').Server(app);
 
-let colors = require('colors/safe');
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', expressHandlebars({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client/index.html');
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'client/'))); // folder for images, css files, etc.
+
+// Express Session
+app.use(expressSession({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+    let namespace = param.split('.');
+    let root = namespace.shift();
+    let formParam = root;
+
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
 
-app.use('/client', express.static(__dirname + '/client'));
+app.use('/', routes);
+app.use('/accounts', accounts);
 
-serv.listen(2000);
-console.log("Server running...");
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/client/index.html');
+// });
 
-let SOCKET_LIST = {};
-let PLAYER_LIST = {}; // list of connected players
-let onlinePlayers = 0;
-/*
-let Player = (id) => {
-  let self = {
-    id: id
-  }
+// app.use('/client', express.static(__dirname + '/client'));
 
-  Player.list[id] = self;
-  return self;
-}
-
-Player.list = {};
-
-Player.onConnect = (socket) => {
-  let player = Player(socket.id);
-}
-
-Player.onDisconnect = (socket) => {
-  delete Player.list[socket.id];
-}
-*/
-
-class Player {
-  constructor(id) {
-    this.id = id;
-  }
-
-  onConnect(socket) {
-    onlinePlayers++;
-    console.log(colors.green('>'), 'Player:', socket.id, 'connected.',
-      colors.yellow('\tOnline:'), onlinePlayers, 'players.');
-  }
-
-  onDisconnect(socket) {
-    onlinePlayers--;
-    console.log(colors.red('<'), 'Player:', socket.id, 'disconnected.',
-      colors.yellow('\tOnline:'), onlinePlayers, 'players.');
-  }
-}
-
-let USERS = {
-  // username::password
-  'david':'david',
-  'tomas':'tomas',
-  'admin':'admin'
-}
-
-let isValidPassword = (data, callback) => {
-  db.account.find({username:data.username, password:data.password}, (error, result) => {
-    // find every account in db
-    if (result.length > 0) // check if in db are any accounts
-      callback(true);
-    else
-      callback(false);
-    // callback(USERS[data.username] === data.password);
-  });
-}
-
-let isUsernameTaken = (data, callback) => {
-  db.account.find({username:data.username}, (error, result) => {
-  // find every account with username in db and check if the username exists in db
-  if (result.length > 0)
-    callback(true);
-  else
-    callback(false);
-    //callback(USERS[data.username]);
-  });
-}
-
-let addUser = (data, callback) => {
-  // insert a new user into the db
-  db.account.insert({username:data.username, password:data.password}, (error) => {
-    callback();
-  });
-}
-
-let io = require('socket.io')(serv, {});
-io.sockets.on('connection', (socket) => {
-  socket.id = Math.random();
-  SOCKET_LIST[socket.id] = socket;
-  // console.log('socket connection:');
-
-  let player = new Player(socket.id);
-
-  socket.on('signIn', (data) => {
-    isValidPassword(data, (res) => {
-      if (res) {
-        // PLAYER_LIST[socket.id] = player;
-        // Player.onConnect(socket);
-        //let player = new Player(socket.id);
-        PLAYER_LIST[socket.id] = player;
-        player.onConnect(socket);
-        socket.emit('signInResponse', {success:true});
-      } else {
-        socket.emit('signInResponse', {success:false});
-      }
-    });
-  });
-
-  socket.on('signUp', (data) => {
-    isUsernameTaken(data, (res) => {
-      if (res) {
-        socket.emit('signUpResponse', {success:false});
-      } else {
-        addUser(data, () => {
-          socket.emit('signUpResponse', {success:true});
-        });
-      }
-    });
-  });
-
-  socket.on('disconnect', () => {
-    delete SOCKET_LIST[socket.id];
-    delete PLAYER_LIST[socket.id];
-    player.onDisconnect(socket);
-    // Player.onDisconnect(socket);
-    //player.onDisconnect(socket.id);
-  });
-
-
-  // socket.on('happy', function(data) {
-  //   console.log(`happy because ${data.reason}`);
-  // });
-
-  // socket.emit('serverMsg', {
-  //   msg: 'Hello'
-  // });
-
+// Set Port
+app.set('port', (process.env.PORT || 2000));
+app.listen(app.get('port'), () => {
+  console.log(colors.green("Server running... ") + "on port " + app.get('port') + "; press " + colors.red("Ctrl-C") + " to terminate.");
 });
+//serv.listen(2000);
+//console.log("Server running...");
+
+// let SOCKET_LIST = {};
+// let PLAYER_LIST = {}; // list of connected players
+// let onlinePlayers = 0;
+// /*
+// let Player = (id) => {
+//   let self = {
+//     id: id
+//   }
+//
+//   Player.list[id] = self;
+//   return self;
+// }
+//
+// Player.list = {};
+//
+// Player.onConnect = (socket) => {
+//   let player = Player(socket.id);
+// }
+//
+// Player.onDisconnect = (socket) => {
+//   delete Player.list[socket.id];
+// }
+// */
+//
+// class Player {
+//   constructor(id) {
+//     this.id = id;
+//   }
+//
+//   onConnect(socket) {
+//     onlinePlayers++;
+//     console.log(colors.green('>'), 'Player:', socket.id, 'connected.',
+//       colors.yellow('\tOnline:'), onlinePlayers, 'players.');
+//   }
+//
+//   onDisconnect(socket) {
+//     onlinePlayers--;
+//     console.log(colors.red('<'), 'Player:', socket.id, 'disconnected.',
+//       colors.yellow('\tOnline:'), onlinePlayers, 'players.');
+//   }
+// }
+//
+// let USERS = {
+//   // username::password
+//   'david':'david',
+//   'tomas':'tomas',
+//   'admin':'admin'
+// }
+//
+// let isValidPassword = (data, callback) => {
+//   db.account.find({username:data.username, password:data.password}, (error, result) => {
+//     // find every account in db
+//     if (result.length > 0) // check if in db are any accounts
+//       callback(true);
+//     else
+//       callback(false);
+//     // callback(USERS[data.username] === data.password);
+//   });
+// }
+//
+// let isUsernameTaken = (data, callback) => {
+//   db.account.find({username:data.username}, (error, result) => {
+//   // find every account with username in db and check if the username exists in db
+//   if (result.length > 0)
+//     callback(true);
+//   else
+//     callback(false);
+//     //callback(USERS[data.username]);
+//   });
+// }
+//
+// let addUser = (data, callback) => {
+//   // insert a new user into the db
+//   db.account.insert({username:data.username, password:data.password}, (error) => {
+//     callback();
+//   });
+// }
+
+// let io = require('socket.io')(serv, {});
+// io.sockets.on('connection', (socket) => {
+//   socket.id = Math.random();
+//   SOCKET_LIST[socket.id] = socket;
+//   // console.log('socket connection:');
+//
+//   let player = new Player(socket.id);
+//
+//   socket.on('signIn', (data) => {
+//     isValidPassword(data, (res) => {
+//       if (res) {
+//         // PLAYER_LIST[socket.id] = player;
+//         // Player.onConnect(socket);
+//         //let player = new Player(socket.id);
+//         PLAYER_LIST[socket.id] = player;
+//         player.onConnect(socket);
+//         socket.emit('signInResponse', {success:true});
+//       } else {
+//         socket.emit('signInResponse', {success:false});
+//       }
+//     });
+//   });
+//
+//   socket.on('signUp', (data) => {
+//     isUsernameTaken(data, (res) => {
+//       if (res) {
+//         socket.emit('signUpResponse', {success:false});
+//       } else {
+//         addUser(data, () => {
+//           socket.emit('signUpResponse', {success:true});
+//         });
+//       }
+//     });
+//   });
+//
+//   socket.on('disconnect', () => {
+//     delete SOCKET_LIST[socket.id];
+//     delete PLAYER_LIST[socket.id];
+//     player.onDisconnect(socket);
+//     // Player.onDisconnect(socket);
+//     //player.onDisconnect(socket.id);
+//   });
+//
+//
+//   // socket.on('happy', function(data) {
+//   //   console.log(`happy because ${data.reason}`);
+//   // });
+//
+//   // socket.emit('serverMsg', {
+//   //   msg: 'Hello'
+//   // });
+//
+// });
